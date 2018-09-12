679542807eecf6e354faa2c10d38effcbced1394
Thomas <sy.thomas.pro@gmail.com>
2018-09-12 10:06:50 +0200

    Add file "Les_variables_PHP.php (PHP)

---
+++ PHP/Les_variables_en _PHP.php
@@ -0,0 +1,14 @@
+<?php
+    //  Variable use
+    $titleMovie = "Indiana Jones and the Last Crusade"; // Variable String
+    $viewMovie = true; // Variable Booléen
+    $dateMovie = 1989; // Variable Integer
+    $noteMovie = 8.3; // Variable Float
+
+    //  Variable display
+    echo $titleMovie ."\n";
+    if ($viewMovie == true) { 
+        echo "I saw the movie" . "\n";}
+    echo $dateMovie ."\n";
+    echo $noteMovie ."\n";
+?>
