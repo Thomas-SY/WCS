8075f25f73d8bbc31949fff76fda6a8fa87d1f0b
Thomas SY <sy.thomas.pro@gmail.com>
2018-09-25 13:51:28 +0200

    Quest "Compose" use of libraries.

---
+++ Composer/public/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomy
+ * Date: 25/09/18
+ * Time: 09:41
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+
+use HelloWorld\SayHello;
+use App\Wcs\Hello;
+
+$hello = new Hello();
+
+echo $hello -> talk(), "<br>";
+
+echo SayHello::world();
---
+++ Composer/src/Wcs/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomy
+ * Date: 25/09/18
+ * Time: 10:32
+ */
+
+namespace App\Wcs;
+
+
+class Hello
+{
+    public function talk(){
+        return "Hello World !";
+    }
+
+}
