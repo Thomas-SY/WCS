cd30556ac905dc3b97062232553521e4cb13c988
Thomas SY <sy.thomas.pro@gmail.com>
2018-10-19 12:03:01 +0200

    Adding the "language discovery" folder.

---
+++ js/Découverte_du_langage/movie.js
@@ -0,0 +1,8 @@
+// Define some variables
+const movieName = 'MON VOISIN TOTORO';
+const movieDirector = 'HAYAO MIYAZAKI';
+const movieReleaseDate = '8 décembre 1999'; 
+const message = movieName + ', ' + 'réalisé par ' + movieDirector + ', ' + 'est sorti le ' + movieReleaseDate + '.';
+
+// Display a popup
+alert(message);
