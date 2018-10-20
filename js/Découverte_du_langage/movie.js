94caee02af140ab6437e42f6581353738e3f764e
Thomas SY <sy.thomas.pro@gmail.com>
2018-10-20 10:40:46 +0200

    Update "movie.js" string date in int.

--- js/Découverte_du_langage/movie.js
+++ js/Découverte_du_langage/movie.js
@@ -1,7 +1,7 @@
 // Define some variables
 const movieName = 'MON VOISIN TOTORO';
 const movieDirector = 'HAYAO MIYAZAKI';
-const movieReleaseDate = '8 décembre 1999'; 
+const movieReleaseDate = 1999; 
 const message = movieName + ', ' + 'réalisé par ' + movieDirector + ', ' + 'est sorti le ' + movieReleaseDate + '.';
 
 // Display a popup
